28c7e9acebf5c0fa5d1d9faaa57db60c97413ceb
junpisa <junpisa.anun@g.swu.ac.th>
2016-06-08 11:49:58 +0700

    Update MainActivity.java

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,32 +1,40 @@
-package com.example.toonz.video;
+package com.example.toonz.testopencv;
 
+
+import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
-import java.io.ByteArrayOutputStream;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 /**
@@ -40,21 +48,44 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
-    private Uri uriVideo;
-    private String videoPath;
-    private ArrayList result;
-    private ArrayList<String> imagesPathList;
-    private Bitmap yourbitmap;
-    private LinearLayout lnrImages;
+    private File photoFile;
+    Mat gray_img;
     ArrayList s;
+    String r;
+    // Storage Permissions variables
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
+//    private GoogleApiClient client;
+    //persmission method.
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton btn_camera = (ImageButton) findViewById(R.id.camera1);
@@ -64,8 +95,22 @@ public class MainActivity extends Activity {
         btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(photoFile));
+                        startActivityForResult(takePictureIntent, 0);
+                    }
+                }
             }
         });
         btn_many.setOnClickListener(new View.OnClickListener() { ///ถ่ายหลายภาพ
@@ -84,69 +129,85 @@ public class MainActivity extends Activity {
         });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Intent in = new Intent(getApplicationContext(), makeocr.class);
         if (resultCode == RESULT_OK) {
             if (requestCode == 0) {///โหมดถ่ายรูปเดียว
-                Bundle extras = data.getExtras();
-                // Get the returned image from extra
-                Bitmap bmp = (Bitmap) extras.get("data");
-                String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
+                r = "";
+                performCrop();
 
-////                /////tesseract ocr
-                String DATA_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tesseract/";
-                File dir = new File(DATA_PATH + "tessdata/");
-                TessBaseAPI baseApi = new TessBaseAPI();
-                baseApi.init(DATA_PATH,"eng");
-                baseApi.setImage(bmp);
-                String recognizedText = baseApi.getUTF8Text();
-                baseApi.end();
-                try { ////ทำให้เป็น file text เพื่อจะนำไปทำเป็นpdf
-                    File f = new File(path,"test.txt");
-                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(f));
-                    outputStreamWriter.write(recognizedText);
-                    outputStreamWriter.close();
-                }
-                catch (IOException e) {
-                    Log.e("123", "File write failed: " + e.toString());
-                }
-
-                in.putExtra("te",recognizedText);
-                startActivity(in);
-            } else {///เลือกภาพ
+            } else if (requestCode == 1) {///เลือกภาพ
                 s = new ArrayList();
                 ClipData clipData = null;
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
                     clipData = data.getClipData();
                 }
-                //Both approach work
-
-                if(clipData == null){
+                if (clipData == null) {
                     s.add(data.getData().toString());
-                    Toast.makeText(getApplicationContext(), "pathpic"+data.getData().toString(), Toast.LENGTH_LONG).show();
-                }else{
-                    for(int i=0; i<clipData.getItemCount(); i++){
+                    Toast.makeText(getApplicationContext(), "pathpic" + data.getData().toString(), Toast.LENGTH_LONG).show();
+                } else {
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
                         Uri uri = clipData.getItemAt(i).getUri();
                         String imagePath = addImageToList(uri);
                         s.add(imagePath);
                     }
                 }
-                in.putExtra("te","1");
+                in.putExtra("te", "1");
                 Bundle args = new Bundle();
-                args.putSerializable("ARRAYLIST",(Serializable)s);
-                in.putExtra("BUNDLE",args);
+                args.putSerializable("ARRAYLIST", (Serializable) s);
+                in.putExtra("BUNDLE", args);
                 startActivity(in);
+
+            } else{ ///จากโหมดถ่ายภาพเดี่ยว แบบcrop ได้
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                gray_img = new Mat();
+                gray_img = Highgui.imread(path + "/ig.jpg", Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+                Mat re = new Mat();
+                Imgproc.adaptiveThreshold(gray_img, re, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 15, 40);
+                Bitmap bm = Bitmap.createBitmap(gray_img.width(), gray_img.height(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(re, bm);
+                    File file2 = new File(path, "gray.jpg");
+
+                    FileOutputStream fos2 = null;
+                    try {
+                        fos2 = new FileOutputStream(file2);
+                        if (fos2 != null) {
+                            bm.compress(Bitmap.CompressFormat.JPEG, 100, fos2);
+                            fos2.flush();
+                            fos2.close();
+                            Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    String DATA_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tesseract/";
+                    File dir = new File(DATA_PATH + "tessdata/");
+                    TessBaseAPI baseApi = new TessBaseAPI();
+                    baseApi.init(DATA_PATH, "eng");
+                    baseApi.setImage(bm);
+                    String recognizedText = baseApi.getUTF8Text();
+                    baseApi.end();
+                    try { ////ทำให้เป็น file text เพื่อจะนำไปทำเป็นpdf
+                        File f = new File(path, "test.txt");
+                        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(f));
+                        outputStreamWriter.write(recognizedText);
+                        outputStreamWriter.close();
+                    } catch (IOException e) {
+                        Log.e("123", "File write failed: " + e.toString());
+                    }
+                    in.putExtra("te", recognizedText);
+                    startActivity(in);
 
             }
         } else {
-            Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Choose you images.", Toast.LENGTH_LONG).show();
         }
     }
+
 
     public void pick() {
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -163,44 +224,35 @@ public class MainActivity extends Activity {
         cursor.close();
         return imagePath;
     }
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.toonz.video/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMG_" + timeStamp + ".png";
+        String storageDir = Environment.getExternalStorageDirectory()
+                + "/ExtractReceipt";
+        File dir = new File(storageDir);
+        if (!dir.exists())
+            dir.mkdir();
+        File image = new File(storageDir + "/" + imageFileName);
+        return image;
     }
+    private void performCrop() {
+        // take care of exceptions
+        try {
+            // call the standard crop action intent (the user device may not
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(Uri.fromFile(photoFile), "image/*");
+            cropIntent.putExtra("crop", "true");
+            cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(new File(Environment.getExternalStorageDirectory().getAbsolutePath())+"/ig.jpg")) );
+            startActivityForResult(cropIntent, 2);
 
-    @Override
-    public void onStop() {
-        super.onStop();
+        }
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.toonz.video/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
+        catch (ActivityNotFoundException anfe) {
+            Toast toast = Toast.makeText(this, "This device doesn't support the crop action!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
 }
